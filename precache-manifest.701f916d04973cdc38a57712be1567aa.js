4b7c5d6b1027b46b2c0627297f2adce8a5d706f3
runlinwang <rwang3@college.harvard.edu>
2022-09-28 22:27:01 -0400

    Updates

---
+++ precache-manifest.701f916d04973cdc38a57712be1567aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "77e2d932dbe8a8e35753",
+    "url": "/when2meet279/static/css/main.ab7f342f.chunk.css"
+  },
+  {
+    "revision": "77e2d932dbe8a8e35753",
+    "url": "/when2meet279/static/js/main.d5a6b548.chunk.js"
+  },
+  {
+    "revision": "00ef4357727d3eee5d91",
+    "url": "/when2meet279/static/js/runtime~main.b0020067.js"
+  },
+  {
+    "revision": "bd043ba53dfcfe9103bd",
+    "url": "/when2meet279/static/js/2.ec3f2757.chunk.js"
+  },
+  {
+    "revision": "e749bf1a2468870c2b5e57828fa85afd",
+    "url": "/when2meet279/index.html"
+  }
+];
